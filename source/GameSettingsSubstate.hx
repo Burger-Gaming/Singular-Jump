c323832ead8df39a01be126a52c090dd4144dbf0
SmashingGoomba <trentrogers60@gmail.com>
2022-09-06 22:26:40 -0600

    working on it slowly

--- source/GameSettingsSubstate.hx
+++ source/GameSettingsSubstate.hx
@@ -3,21 +3,23 @@ package;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.addons.display.FlxBackdrop;
+import flixel.group.FlxSpriteGroup;
 
 class GameSettingsSubstate extends FlxSubState
 {
 	var backdrop:FlxBackdrop;
+	var testVar:Int = 95;
 
 	override public function new()
 	{
-		super(0x00000000);
+		super();
 	}
 
 	override function create()
 	{
 		super.create();
 
-		backdrop = new FlxBackdrop(GameTools.getImage('menu/difficulty/gameSettings/settingsBackdrop'), 1, 1, true, true);
+		backdrop = new FlxBackdrop(GameTools.getImage('menu/difficulty/gameSettings/settingBackdrop'), 1, 1, true, true);
 		backdrop.velocity.set(50, 50);
 		backdrop.alpha = 0;
 		add(backdrop);
